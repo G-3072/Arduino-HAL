f10a00535af5305e8c49e1c6d8b488cb26b055e5
Gaudenz Schweizer <gaudenz.schweizer@cedes.com>
2025-02-27 13:17:51 +0100

    wrote linker script

--- package/main/src/startup_ATMega328p.c
+++ package/main/src/startup_ATMega328p.c
@@ -2,21 +2,24 @@
 
 #define FLASH_START     0x0000U
 #define FALSH_SIZE      (32 * 1024) // 32kB of
-#define FLASH_END       ((FLASH_START) + (FLASH_END))
+#define FLASH_END       (FLASH_START + FALSH_SIZE)
 
-#define SRAM_START      0x0100U
+#define SRAM_OFFSET     0x800000U
+#define SRAM_START      (0x0100U + SRAM_OFFSET)
 #define SRAM_SIZE       (2 * 1024) //2kB of SRAM
-#define SRAM_END        ((SRAM_START) + (SRAM_SIZE))
+#define SRAM_END        (SRAM_START + SRAM_SIZE)
 
 #define STACK_START     SRAM_END
 
-void reset_handler(void){
+void Reset_Handler(void);
 
-
-    main();
-}
-
-uint16_t vectorTable[] = {
-    (uint64_t) &reset_handler,
+uint16_t isr_vector[] __attribute__((section(".isr_vector"))) = {   //interupt vector table
+    (uint16_t) &Reset_Handler,
 };
 
+void main(void);
+extern uint32_t _stext, _etext, _sdata, _edata, _sbss, _ebss, _srodata, _erodata, _sidata;
+
+void Reset_Handler(void){
+
+}
